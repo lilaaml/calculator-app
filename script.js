f94983c354d67fb156394eace1e7c3b52da3959e
Lila Amelia <lilaamelia77@gmail.com>
2024-04-12 10:08:21 +0700

    fixed integer decimal error

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ function clearScreen() {
     screen.value = "";
 }
 
+function deleteInput() {
+    screen.value = (screen.value).toString().slice(0, -1);
+}
+
 function calculate() {
     try {
-        screen.value = eval(screen.value).toFixed(2);
+        if(eval(screen.value) % 1 != 0) {
+            screen.value = (eval(screen.value)).toFixed(2);
+        } else {
+            screen.value = eval(screen.value);
+        }
     } catch (error) {
         screen.value = "Error";
     }
